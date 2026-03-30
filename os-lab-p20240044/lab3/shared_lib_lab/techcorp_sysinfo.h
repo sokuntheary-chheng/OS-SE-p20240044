2778fa73ab5806b3296897a3266bed6a5fdfd076
Chheng Sokuntheary <chhengsokuntheary572@gmail.com>
2026-03-30 06:55:27 +0000

    Lab 3 Task5 completed

---
+++ os-lab-p20240044/lab3/shared_lib_lab/techcorp_sysinfo.h
@@ -0,0 +1,13 @@
+#infndef TECHCORP_SYSINFO_H
+#define TECHCORP_SYSINFO_H
+
+//Return the hostname of the machine
+const char* tc_get_hostname(void);
+
+//Return the current uptime in a human-readable string
+const char* tc_get_uptime(void);
+
+//Returns the number of CPU cores 
+int tc_get_cpu_count(void);
+
+#endif
